5ff7cec87822c61484c01fa16988edd467aef619
Cristian Valencia G <117423462+cvalenciapatagonia@users.noreply.github.com>
2023-09-04 11:26:42 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Desafio-3-React-1/',
+  base: 'Desafio-3-React-1',
 });
